fd7c2df9dbe6e68af12b551e6af618bad1c753b4
Rishabh Deshmukh <rishabh.deshmukh@csa.iisc.ernet.in>
2016-08-24 19:45:07 +0530

    all passed except 16

--- src/e0210/Analysis.java
+++ src/e0210/Analysis.java
@@ -13,6 +13,9 @@ import java.util.*;
  */
 
 import java.util.Map;
+
+import javax.management.BadBinaryOpValueExpException;
+
 import java.util.*;
 import soot.*;
 import soot.jimple.*;
@@ -28,155 +31,86 @@ import java.io.*;
 public class Analysis extends BodyTransformer {
 
 	
-	/* 
-	  static SootClass counterClass;
-	  static SootMethod increaseCounter, reportCounter;
-
-	  static {
-	    counterClass    = Scene.v().loadClassAndSupport("MyCounter");
-	    increaseCounter = counterClass.getMethod("void increase(int)");
-	    reportCounter   = counterClass.getMethod("void report()");
-	  }
-	*/
+	SootField globalC;
+	boolean added=false;
 	@Override
 	protected void internalTransform(Body b, String phaseName, Map<String, String> options) {
 		
-		
+		if (added)
+            globalC = Scene.v().getMainClass().getFieldByName("globalC");
+        else
+        {
+            // Add gotoCounter field
+        	globalC = new SootField("globalC", LongType.v(),Modifier.STATIC);
+            Scene.v().getMainClass().addField(globalC);
+            added=true;
+        }
 		
 		
 		PatchingChain<Unit> byteU=b.getUnits();
-		Local dynBranch,disp;
-		
-				
+		Local dynBranch,disp,gL;
 		
 		
-		dynBranch=Jimple.v().newLocal("dynBranch", LongType.v());
+		
+		gL=Jimple.v().newLocal("gL", LongType.v());			//local, that will hold global value in each method
+		b.getLocals().add(gL);
+		
+		dynBranch=Jimple.v().newLocal("dynBranch", LongType.v());		//local variable
 		b.getLocals().add(dynBranch);
 		
-		disp=Jimple.v().newLocal("disp", RefType.v("java.io.PrintStream"));
+		disp=Jimple.v().newLocal("disp", RefType.v("java.io.PrintStream"));	// for printing
 		b.getLocals().add(disp);
 		
 		byteU.insertAfter(Jimple.v().newAssignStmt(dynBranch, LongConstant.v(0)), byteU.getFirst());
 		
+		byteU.insertAfter(Jimple.v().newAssignStmt(gL, Jimple.v().newStaticFieldRef(globalC.makeRef())), byteU.getFirst());  
 		
-		Iterator iter=byteU.snapshotIterator();
+		Iterator iter=byteU.snapshotIterator();				
 			
 			while(iter.hasNext()){
 				Stmt s=(Stmt)iter.next();
 				if(s instanceof IfStmt){
 					byteU.insertBefore(Jimple.v().newAssignStmt(dynBranch, Jimple.v().newAddExpr(dynBranch, LongConstant.v(1))), s);
+					byteU.insertBefore(Jimple.v().newAssignStmt(gL, Jimple.v().newAddExpr(gL, LongConstant.v(1))), s);
 					// Here i have to insert increment statement to count Dyn branching
 				}
-				else if(s instanceof ReturnVoidStmt || s instanceof ReturnStmt ){		//InvokeStmt to handle System.exit(0) "|| s instanceof InvokeStmt"
+				else if(s instanceof ReturnVoidStmt || s instanceof ReturnStmt ){		
 					byteU.insertBefore(Jimple.v().newAssignStmt(disp, Jimple.v().newStaticFieldRef(Scene.v().getField("<java.lang.System: java.io.PrintStream out>").makeRef())),s);
-
-					
-					SootMethod toCall = Scene.v().getMethod("<java.io.PrintStream: void println(long)>");
-		            byteU.insertBefore(Jimple.v().newInvokeStmt(Jimple.v().newVirtualInvokeExpr(disp, toCall.makeRef(), dynBranch)),s);
+							
+		            byteU.insertBefore(Jimple.v().newInvokeStmt(Jimple.v().newVirtualInvokeExpr(disp, Scene.v().getMethod("<java.io.PrintStream: void println(long)>").makeRef(), dynBranch)),s);
 		            
+		            byteU.insertBefore(Jimple.v().newAssignStmt(Jimple.v().newStaticFieldRef(globalC.makeRef()),gL), s); //on return assign the value of gL to globalC
 		            
-		            
-		            SootMethod toCall1 = Scene.v().getMethod("<java.io.PrintStream: void print(long)>");
-		            byteU.insertBefore(Jimple.v().newInvokeStmt(Jimple.v().newVirtualInvokeExpr(disp, toCall1.makeRef(), dynBranch)),s);
-					
-				}
+		            if(b.getMethod().getSubSignature().equals("void main(java.lang.String[])"))            	
+		            	byteU.insertBefore(Jimple.v().newInvokeStmt(Jimple.v().newVirtualInvokeExpr(disp, Scene.v().getMethod("<java.io.PrintStream: void print(long)>").makeRef(),gL )),s);
+		           				
+				}	// handled all types of returns
+				
 				else if (s.containsInvokeExpr()){
 					InvokeExpr exp=(InvokeExpr)s.getInvokeExpr();
 					String str=exp.toString();
 					if(str.contains("staticinvoke <java.lang.System: void exit(")){			//In this implementation there cant be any conflict in matching func. name
 						
 						byteU.insertBefore(Jimple.v().newAssignStmt(disp, Jimple.v().newStaticFieldRef(Scene.v().getField("<java.lang.System: java.io.PrintStream out>").makeRef())),s);
-
 						
-						SootMethod toCall = Scene.v().getMethod("<java.io.PrintStream: void println(long)>");
-			            byteU.insertBefore(Jimple.v().newInvokeStmt(Jimple.v().newVirtualInvokeExpr(disp, toCall.makeRef(), dynBranch)),s);
+						byteU.insertBefore(Jimple.v().newInvokeStmt(Jimple.v().newVirtualInvokeExpr(disp, Scene.v().getMethod("<java.io.PrintStream: void println(long)>").makeRef(), dynBranch)),s);
 			            
-			            
-			            SootMethod toCall1 = Scene.v().getMethod("<java.io.PrintStream: void print(long)>");
-			            byteU.insertBefore(Jimple.v().newInvokeStmt(Jimple.v().newVirtualInvokeExpr(disp, toCall1.makeRef(), dynBranch)),s);
-					}
+			            byteU.insertBefore(Jimple.v().newInvokeStmt(Jimple.v().newVirtualInvokeExpr(disp, Scene.v().getMethod("<java.io.PrintStream: void print(long)>").makeRef(), gL)),s);
+					}    	//handled exit(0) and
 					
+					else if(str.contains("staticinvoke <")){
+						byteU.insertBefore(Jimple.v().newAssignStmt(Jimple.v().newStaticFieldRef(globalC.makeRef()),gL), s);
+						byteU.insertAfter(Jimple.v().newAssignStmt(gL,Jimple.v().newStaticFieldRef(globalC.makeRef())), s);
+											
+					}	//handled static function calls					
 						
 				}
 				
-			}
+			}	//instrumentation done
 			
-			
-			/*
-			byteU.insertBefore(Jimple.v().newAssignStmt(disp, Jimple.v().newStaticFieldRef(Scene.v().getField("<java.lang.System: java.io.PrintStream out>").makeRef())),byteU.getLast());
-
-			
-			SootMethod toCall = Scene.v().getMethod("<java.io.PrintStream: void println(long)>");
-            byteU.insertBefore(Jimple.v().newInvokeStmt(Jimple.v().newVirtualInvokeExpr(disp, toCall.makeRef(), dynBranch)),byteU.getLast());
-            
-            
-            SootMethod toCall1 = Scene.v().getMethod("<java.io.PrintStream: void print(long)>");
-            byteU.insertBefore(Jimple.v().newInvokeStmt(Jimple.v().newVirtualInvokeExpr(disp, toCall1.makeRef(), dynBranch)),byteU.getLast());
-		
-			*/
-		
-		//----------------------------------------
-		
-		/*
-		
-		PatchingChain<Unit> units = b.getUnits();
-        Local arg, tmpRef,cnt;
-        
-        
-        cnt=Jimple.v().newLocal("cnt", LongType.v());
-        b.getLocals().add(cnt);
-        Stmt countInit=Jimple.v().newAssignStmt(cnt, LongConstant.v(0));	//Initializing new count variable
-        units.insertAfter(countInit, units.getFirst());		//inserting count(cnt) variable
-       
-        
-        Stmt ass= Jimple.v().newAssignStmt(cnt, Jimple.v().newAddExpr(cnt, LongConstant.v(1)));
-        units.insertBefore(ass, units.getLast());
-        
-        
-        // Add some locals, java.lang.String l0   It's a String Object
-            arg = Jimple.v().newLocal("l0", ArrayType.v(RefType.v("java.lang.String"), 1));
-            b.getLocals().add(arg);
-        
-        // Add locals, java.io.printStream tmpRef
-            tmpRef = Jimple.v().newLocal("tmpRef", RefType.v("java.io.PrintStream"));
-            b.getLocals().add(tmpRef);
-            
-        // add "l0 = @parameter0"   It can't be inserted into "main" because there is another string variable accesing @parameter0
-            units.insertBefore(Jimple.v().newIdentityStmt(arg, 
-                 Jimple.v().newParameterRef(ArrayType.v(RefType.v("java.lang.String"), 1), 0)),units.getLast());
-        	
-        // add "tmpRef = java.lang.System.out"
-            units.insertBefore(Jimple.v().newAssignStmt(tmpRef, Jimple.v().newStaticFieldRef(
-                Scene.v().getField("<java.lang.System: java.io.PrintStream out>").makeRef())),units.getLast());
-        
-        // insert "tmpRef.println("Hello world!")"
-        
-            SootMethod toCall = Scene.v().getMethod("<java.io.PrintStream: void println(java.lang.String)>");
-            units.insertBefore(Jimple.v().newInvokeStmt(Jimple.v().newVirtualInvokeExpr(tmpRef, toCall.makeRef(), StringConstant.v("Hello world!"))),units.getLast());
-            
-           
-            Iterator stmtIt = b.getUnits().snapshotIterator();
-            		while (stmtIt.hasNext())
-    				{	Stmt s = (Stmt) stmtIt.next();
-            			if (s instanceof IfStmt){
-            			Stmt newas=Jimple.v().newAssignStmt(cnt, LongConstant.v(1111111));	
-            			units.insertAfter(newas, s);
-    				    
-            			}
-            		
-    				    
-    				    
-    				} 
-    		System.out.println(b.toString());
-		
-	*/	
-			
+			System.out.println(b.toString());
 			
 		
 		return;
 	}
-
-	
-	
-
 }
