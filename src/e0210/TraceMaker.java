ad562e6455bba62794ee15fd3175c8f9f7c7d898
Rishabh Deshmukh <rishabh.deshmukh@csa.iisc.ernet.in>
2016-11-03 18:41:42 +0530

    P3: Begin Solver

--- src/e0210/TraceMaker.java
+++ src/e0210/TraceMaker.java
@@ -15,6 +15,13 @@ import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.Iterator;
+import java.util.List;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.DirectedWeightedPseudograph;
 
 import soot.PackManager;
 import soot.Transform;
@@ -25,36 +32,102 @@ public class TraceMaker {
 
 		String project = args[0];
 		String testcase = args[1];
-
-		// Input args that were given to the testcase executable. 
-		// These may be need during symbolic execution
-		String argsFile = "Testcases/" + project + "/input/" + testcase;
-		String testcaseArgs = new String(Files.readAllBytes(Paths.get(argsFile)));
-
-        // The raw output from instrumented code. You'll use this to construct the tuple for each method call
+		
+		try{
+		File f=new File("Testcases/"+project+"/tuples");
+		if(f.exists()) f.delete();
+		f.mkdir();
+		}catch(Exception e){}
+		
+		// The raw output from instrumented code. You'll use this to construct the tuple for each method call
 		String inPath = "Testcases/" + project + "/output/" + testcase;
-
-        // The output files for global trace and tuples. You'll output the results to these files
+		// The output files for global trace and tuples. You'll output the results to these files
 		String globalTraceOutPath = "Testcases/" + project + "/processed-output/" + testcase + "-global-trace";
-		String tupleOutPath = "Testcases/" + project + "/processed-output/" + testcase + "-tuples";
+		String tupleOutPath = "Testcases/" + project + "/tuples/"+ testcase;
 
 		System.out.println("Processing " + testcase + " of " + project);
 
 		// Read the contents of the output file into a string
 		String in = new String(Files.readAllBytes(Paths.get(inPath)));
-
-		/*
-		 * 
-		 * Write your algorithm which does the post-processing of the output
-		 * to construct the tuple for each method call
-		 * 
-		 */
-
-		// Call to the symbolic execution.
-		// You can pass any data that's required for symbolic execution
-		// Example: the tuples for each method call
+		Essentials obj1=new Essentials();
+		
+		String[] blFile=in.split(System.getProperty("line.separator"));		//System.getProperty("line.separator")
+		String path=null,result="";
+		DirectedWeightedPseudograph<Vertex, DefaultWeightedEdge> mjGraph=null;
+		List<DirectedWeightedPseudograph<Vertex, DefaultWeightedEdge>> listOfGraph=new ArrayList<DirectedWeightedPseudograph<Vertex, DefaultWeightedEdge>>();
+				
+		try{	
+			FileInputStream f_g_in=new FileInputStream("sootOutput/gr.ser");
+			ObjectInputStream o_g_in=new ObjectInputStream(f_g_in);
+			
+			try {
+				mjGraph=(DirectedWeightedPseudograph<Vertex, DefaultWeightedEdge>) o_g_in.readObject();			
+				while(mjGraph!=null){
+					listOfGraph.add(mjGraph);
+					mjGraph=(DirectedWeightedPseudograph<Vertex, DefaultWeightedEdge>) o_g_in.readObject();				
+					}		
+				o_g_in.close();
+				} 
+				catch (Exception e) {/**e.printStackTrace();*/}
+				
+				f_g_in.close();
+			}
+			catch(Exception e){/*e.printStackTrace();*/}
+		
+		
+		DirectedWeightedPseudograph<Vertex, DefaultWeightedEdge> tempGraph=null;
+		List<String> pathList=new ArrayList<>();
+		Iterator<DirectedWeightedPseudograph<Vertex, DefaultWeightedEdge>> graphListIt;
+		
+		String finalResult="";					//contains the final stream of complete tuples
+		for(String s:blFile){
+			String[] tupleStr=s.split(",");		//split tuple into four string by ","
+			String blStr=tupleStr[3];			//retrieve last component(bl id) from tuple
+			String[] blId=blStr.split("&");		// split bl id string of each tuple into singleton bl-id using "&"
+			result="";							//contains block id's corresponding to bl id's of each tuple  
+			boolean isFirst=true;
+			for(String bls:blId){				//for each bl-id retrieve the full path
+				
+				int bl=Integer.parseInt(bls);
+			graphListIt=listOfGraph.iterator();
+			//BufferedReader br=new BufferedReader(new StringReader(s));
+			
+			int checkBl= Integer.parseInt(bls);		//extract BLid for checking purpose
+			while(graphListIt.hasNext()){
+				tempGraph=graphListIt.next();
+						
+				Vertex checkVert = obj1.getVertexfromInt(tempGraph,0);
+				
+				
+				//Vertex checkVert=mjGraph.vertexSet().iterator().next();		//Extract first vertex to check sBL and eBL
+				if(checkVert.sBL<=checkBl && checkVert.eBl>=checkBl){
+					path=obj1.regeneratePath(tempGraph, bl,checkVert.sBL);	//block id path corresponding to each bl id(single number)
+					if(isFirst){
+						result=result+path;						
+						isFirst=false;
+					}
+					else{
+						result=result+"&"+path;						
+					}
+					
+					break;
+					
+				}
+				
+			}
+			
+			
+		}
+			finalResult=finalResult+tupleStr[0]+","+tupleStr[1]+","+tupleStr[2]+","+result+"\n";
+			
+		}	
+		// Write the tuples of each method call to the output file
+		PrintWriter out = new PrintWriter(tupleOutPath);
+		out.print(finalResult);		
+		out.close();
+		
+		//----------Tuple Written Successfully--------//
 		sootMainSymbolicExecution(project, testcase);
-
         /*
             You should have the intra-thread trace for each thread and the
             path constraints by now.
@@ -90,9 +163,11 @@ public class TraceMaker {
 
 		tupleWriter.close();
 
+
 		return;
 	}
 
+	
 	public static void sootMainSymbolicExecution(String project, String testcase) {
 
 		ArrayList<String> base_args = new ArrayList<String>();
@@ -140,9 +215,9 @@ public class TraceMaker {
 		base_args.add("-output-dir");
 		base_args.add("Testcases/" + project + "/sootBin/");
 
-		SymbolicExecution obj = new SymbolicExecution();
+		SymbolicExecution obj = new SymbolicExecution(project,testcase);
 
-		PackManager.v().getPack("jtp").add(new Transform("jtp.MyAnalysis", obj));
+		PackManager.v().getPack("wjtp").add(new Transform("wjtp.MyAnalysis", obj));
 
 		soot.Main.main(base_args.toArray(new String[base_args.size()]));
 
